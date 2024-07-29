4496480f78bea005219c1d99614e3e0a54614234
hari-shadow <hariharanlaligam@gmail.com>
2024-07-29 13:51:27 +0530

    add the second line to the file

--- Stater.java
+++ Stater.java
@@ -1,5 +1,6 @@
 public class Stater {
   public static void public static void main(String[] args) {
     System.out.println("the first line");
+    System.out.println("added the second line");
   }
 }
