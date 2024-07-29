cbfd1579c0f5f5a487ee3beb3299d0df0e4fdbb7
Hariharan Sridhar <103127364+hari-shadow@users.noreply.github.com>
2024-07-29 14:09:18 +0530

    Update Stater.java fetch second line

--- Stater.java
+++ Stater.java
@@ -3,5 +3,6 @@ public class Stater {
     System.out.println("the first line");
     System.out.println("added the second line");
     System.out.println("for the git fetch");
+    System.out.println("git fetch second line");
   }
 }
