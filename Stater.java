0d0a053f89716e31e0ee3af9944ea545151cc26a
Hariharan Sridhar <103127364+hari-shadow@users.noreply.github.com>
2024-07-29 14:05:46 +0530

    Update Stater.java for the git frtch

--- Stater.java
+++ Stater.java
@@ -2,5 +2,6 @@ public class Stater {
   public static void public static void main(String[] args) {
     System.out.println("the first line");
     System.out.println("added the second line");
+    System.out.println("for the git fetch");
   }
 }
